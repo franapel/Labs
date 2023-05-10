77de110d06125cce5737419ff20d3675d3061621
fperez <fperez@wholemeaning.com>
2023-05-09 22:54:47 -0400

    Forzar env vars de postgres

--- src/backend/labsadmin/settings.py
+++ src/backend/labsadmin/settings.py
@@ -226,7 +226,16 @@ else:
         }
     }
     
-DATABASES = config
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),            
+        'PORT': os.getenv('PORT'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
